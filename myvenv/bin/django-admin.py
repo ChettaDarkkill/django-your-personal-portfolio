246b75fecae9e721591703629ab4350a18dc7aed
chetta chetta chetta <>
2019-05-26 01:45:52 +0700

    add env

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chettachettachetta/Documents/Labs/Python/Site #2 - Your Personal Portfolio/myvenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
