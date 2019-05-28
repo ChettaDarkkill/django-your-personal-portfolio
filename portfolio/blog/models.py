ba3768bb54013e6957478c6558459a4766b9b057
chetta chetta chetta <>
2019-05-28 21:07:32 +0700

    finist bootstrap

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Create A Blog models
+# title
+# pub_date
+# body
+# image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# Add the Blog app to the settings
+
+
+# create a migration
+
+# Migration
+
+
+# Add to admin
